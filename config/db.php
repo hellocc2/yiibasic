9e4a12507abfb5b8628d787c3444677b40a63d5d
root <root@localhost.localdomain>
2017-10-12 20:46:07 +0800

    basic

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.68.128;dbname=test',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
